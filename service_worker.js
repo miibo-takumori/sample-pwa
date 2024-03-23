e2e38e4dc7a6a42094b0c9d4a3ae7729c3e47124
miibo-takumori <takumori@miibo.jp>
2024-03-23 22:25:14 +0900

    fix

--- service_worker.js
+++ service_worker.js
@@ -5,21 +5,19 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+    event.waitUntil(caches
+        .open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
+    event.respondWith(caches
+        .match(event.request)
+        .then(function(response) {
+            return response ? response : fetch(event.request);
+        })
     );
 });
