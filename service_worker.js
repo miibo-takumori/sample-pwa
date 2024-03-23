bd00126543a902d429af3053bcbb05bec6cf761d
miibo-takumori <takumori@miibo.jp>
2024-03-23 22:11:52 +0900

    init

---
+++ service_worker.js
@@ -0,0 +1,25 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    'miibo-takumori.gihub.io/sample-pwa',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
